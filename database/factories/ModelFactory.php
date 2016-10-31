389ab4fededcf3ecb339a480f0907e115318c39c
Mher Margaryan <margaryan.mher.28@gmail.com>
2016-11-01 03:46:10 +0400

    Pagination ready

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,20 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+// $factory->define(App\User::class, function (Faker\Generator $faker) {
+//     return [
+//         'name' => $faker->name,
+//         'email' => $faker->safeEmail,
+//         'password' => bcrypt(str_random(10)),
+//         'remember_token' => str_random(10),
+//     ];
+// });
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+        'created_at'=>$faker->dateTime,
+        'updated_at'=>$faker->dateTime,
     ];
 });
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
 	@section('content')
 		<div class="row">
 			<div class="col-md-10">
-				<h1>All posts</h1>
+				<h1 class="page-header">All posts</h1>
 			</div>
 			<div class="col-md-2">
 				<a href="{{route('posts.create')}}" class="btn btn-primary btn-lg btn-block">Create new post</a>
@@ -39,11 +39,11 @@
 							<td><a href="{{route('posts.edit', $post->id)}}" class="btn btn-default btn-sm">Edit</a></td>
 						</tr>
 				    @endforeach()
-				      <tr>
-				        
-				      </tr>
 				    </tbody>
 				  </table>
+				  <div class="text-center">
+				  	{!! $posts->links() !!}
+				  </div>
 			</div>
 		</div>
 	@endsection()
